ef5a409447b3c3d7b757be46eaca7515999d9c9d
Gouravkore7 <100783356+Gouravkore7@users.noreply.github.com>
2026-05-07 15:27:12 +0530

    Remove extra blank lines in test_generate_hash

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,16 +1,12 @@
 import pandas as pd
 
 from pipeline.utils import generate_hash
-
-
 def test_generate_hash():
-
     row = pd.Series({
         "id": 1,
         "amount": 500
     })
 
     result = generate_hash(row)
-
     assert result is not None
     assert isinstance(result, str)
