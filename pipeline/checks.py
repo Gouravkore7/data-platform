d27b2f74ea2211df1d3c3cf588779969f765fc0d
Gouravkore7 <100783356+Gouravkore7@users.noreply.github.com>
2026-05-07 15:32:35 +0530

    Refactor check_null_customer_ids function

--- pipeline/checks.py
+++ pipeline/checks.py
@@ -1,9 +1,10 @@
+
 from dagster import (
     asset_check,
     AssetCheckResult
 )
-
 from connectors.postgres_connector import PostgresConnector
+
 
 
 @asset_check(asset="gold_risk_dataset")
@@ -12,13 +13,10 @@ def check_null_customer_ids():
     postgres = PostgresConnector(
         "postgresql://admin:admin@localhost:5432/data_platform"
     )
-
     df = postgres.read(
         "SELECT * FROM gold_risk_dataset"
     )
-
     passed = df["customer_id"].isnull().sum() == 0
-
     return AssetCheckResult(
         passed=passed
     )
