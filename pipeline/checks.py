a65d37620ce762558a057542e06f3adaf4bb2281
Gouravkore7 <100783356+Gouravkore7@users.noreply.github.com>
2026-05-07 16:19:45 +0530

    Update checks.py

--- pipeline/checks.py
+++ pipeline/checks.py
@@ -1,10 +1,9 @@
-
 from dagster import (
     asset_check,
     AssetCheckResult
 )
-from connectors.postgres_connector import PostgresConnector
 
+from connectors.postgres_connector import PostgresConnector
 
 
 @asset_check(asset="gold_risk_dataset")
@@ -13,10 +12,15 @@ def check_null_customer_ids():
     postgres = PostgresConnector(
         "postgresql://admin:admin@localhost:5432/data_platform"
     )
+
     df = postgres.read(
         "SELECT * FROM gold_risk_dataset"
     )
-    passed = df["customer_id"].isnull().sum() == 0
+
+    passed = bool(
+        df["customer_id"].isnull().sum() == 0
+    )
+
     return AssetCheckResult(
         passed=passed
     )
