ed813f6b3102254c608fcb0adb7f098515a351c1
Gouravkore7 <100783356+Gouravkore7@users.noreply.github.com>
2026-05-07 15:31:32 +0530

    Refactor bronze_query1 function for clarity

--- pipeline/bronze_assets.py
+++ pipeline/bronze_assets.py
@@ -1,34 +1,24 @@
 from dagster import asset
 import pandas as pd
-
 from connectors.oracle_connector import OracleConnector
 from connectors.postgres_connector import PostgresConnector
-
 from pipeline.utils import generate_hash
+
 
 
 @asset
 def bronze_query1():
+    oracle = OracleConnector("data/query1.csv")        #Read the CSV file
 
-    # Read CSV (mock Oracle)
-    oracle = OracleConnector("data/query1.csv")
-
-    # Connect Postgres
     postgres = PostgresConnector(
-        "postgresql://admin:admin@localhost:5432/data_platform"
+        "postgresql://admin:admin@localhost:5432/data_platform"           #connect postgre
     )
+    
+    df = oracle.fetch_data()   #load data in dataframe
+    df["ingestion_ts"] = pd.Timestamp.now()    #ingestion timestamp
+    df["row_hash"] = df.apply(generate_hash, axis=1)    #Create row hash
 
-    # Load data
-    df = oracle.fetch_data()
-
-    # Add ingestion timestamp
-    df["ingestion_ts"] = pd.Timestamp.now()
-
-    # Generate row hash
-    df["row_hash"] = df.apply(generate_hash, axis=1)
-
-    # Incremental loading
-    try:
+    try:                 #handle incremental load with exception
         existing = postgres.read(
             "SELECT row_hash FROM bronze_query1"
         )
@@ -36,11 +26,8 @@ def bronze_query1():
         df = df[
             ~df["row_hash"].isin(existing["row_hash"])
         ]
-
     except:
         pass
-
-    # Write to postgres
-    postgres.write(df, "bronze_query1")
+    postgres.write(df, "bronze_query1")    # Write to postgre
 
     return df
