18b4f9d4f05cd241c0770cbec122dc03a0424e5b
Gouravkore7 <100783356+Gouravkore7@users.noreply.github.com>
2026-05-07 15:35:28 +0530

    Refactor gold_assets.py to streamline code

--- pipeline/gold_assets.py
+++ pipeline/gold_assets.py
@@ -3,15 +3,12 @@ import pandas as pd
 
 from connectors.postgres_connector import PostgresConnector
 
-
 @asset(deps=["silver_query1"])
 def gold_risk_dataset():
 
-    postgres = PostgresConnector(
-        "postgresql://admin:admin@localhost:5432/data_platform"
-    )
+    postgres = PostgresConnector("postgresql://admin:admin@localhost:5432/data_platform")
 
-    df = postgres.read(
+    df = postgres.read(                        #read the tranformed data
         "SELECT * FROM silver_query1"
     )
 
@@ -22,18 +19,15 @@ def gold_risk_dataset():
         "amount": "sum",
         "date": "max"
     }).reset_index()
-
     agg.columns = [
         "customer_id",
         "frequency",
         "monetary",
         "last_transaction_date"
     ]
-
     agg["recency_days"] = (
         pd.Timestamp.now() - agg["last_transaction_date"]
     ).dt.days
-
     postgres.write(
         agg,
         "gold_risk_dataset"
@@ -41,22 +35,16 @@ def gold_risk_dataset():
 
     return agg
 
-
 @asset(deps=["silver_query1"])
 def gold_analytics_dataset():
-
     postgres = PostgresConnector(
         "postgresql://admin:admin@localhost:5432/data_platform"
     )
-
     df = postgres.read(
         "SELECT * FROM silver_query1"
     )
-
     df["date"] = pd.to_datetime(df["date"])
-
     df["month"] = df["date"].dt.to_period("M").astype(str)
-
     monthly_summary = df.groupby("month").agg({
         "amount": "sum",
         "transaction_id": "count"
