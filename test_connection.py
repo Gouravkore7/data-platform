214c5d80df77f48596e9c926734313e5375d610f
Gouravkore7 <100783356+Gouravkore7@users.noreply.github.com>
2026-05-07 15:25:43 +0530

    Refactor connection initialization in test_connection.py
    Removed unnecessary line breaks in connection setup.

--- test_connection.py
+++ test_connection.py
@@ -1,15 +1,10 @@
 from connectors.postgres_connector import PostgresConnector
 import pandas as pd
 
-conn = PostgresConnector(
-    "postgresql://admin:admin@localhost:5432/data_platform"
-)
-
+conn = PostgresConnector("postgresql://admin:admin@localhost:5432/data_platform")
 df = pd.DataFrame({
     "id": [1, 2],
     "name": ["A", "B"]
 })
-
 conn.write(df, "test_table")
-
 print("SUCCESS")
