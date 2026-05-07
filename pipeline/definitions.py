6d08520ab21658f465b472d7ac321666e8bbb1ed
Gouravkore7 <100783356+Gouravkore7@users.noreply.github.com>
2026-05-07 15:33:41 +0530

    Add bronze_query1 import to definitions.py
    Added a new import for bronze_query1 in the definitions file.

--- pipeline/definitions.py
+++ pipeline/definitions.py
@@ -1,6 +1,7 @@
 from dagster import Definitions
 
 from pipeline.bronze_assets import bronze_query1
+
 from pipeline.silver_assets import silver_query1
 
 from pipeline.gold_assets import (
@@ -16,22 +17,18 @@ from pipeline.checks import check_null_customer_ids
 
 
 defs = Definitions(
-
     assets=[
         bronze_query1,
         silver_query1,
         gold_risk_dataset,
         gold_analytics_dataset
     ],
-
     schedules=[
         daily_schedule
     ],
-
     sensors=[
         file_sensor
     ],
-
     asset_checks=[
         check_null_customer_ids
     ]
